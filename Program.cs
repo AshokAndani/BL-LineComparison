6fcf07454fe23c57f6ca8c293d6131e5c820b2a9
ashokk <ashokk@nextgen.com>
2022-11-26 20:34:30 +0530

    code changes for UC2

--- Program.cs
+++ Program.cs
@@ -11,20 +11,51 @@ namespace LineComparison
         }
         public static void LenghtofLine()
         {
+            Console.WriteLine("Enter the Values of First Line");
+
             Console.WriteLine("Enter First Point");
-            Console.Write("X1=");
+            Console.WriteLine("X1=");
             double x1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Y1=");
+            Console.WriteLine("Y1=");
             double y1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("........................");
+
             Console.WriteLine("Enter Second Point");
-            Console.Write("X2=");
+            Console.WriteLine("X2=");
             double x2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Y2=");
+            Console.WriteLine("Y2=");
             double y2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("........................");
-            double LenghtofLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.Write("Lenght of a Line is : {0}", LenghtofLine);
+            Console.WriteLine("........................");
+
+            Console.WriteLine("Enter the Values of Second Line");
+
+            Console.WriteLine("Enter First Point");
+            Console.WriteLine("X3=");
+            double x3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Y3=");
+            double y3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("........................");
+
+            Console.WriteLine("Enter Second Point");
+            Console.WriteLine("X4=");
+            double x4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Y4=");
+            double y4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("........................");
+
+            double LenghtofLineOne = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Lenght of First Line is : {0}", LenghtofLineOne);
+            Console.WriteLine("........................");
+            double LenghtofLineTwo = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+            Console.WriteLine("Lenght of Second Line is : {0}", LenghtofLineTwo);
+            if (LenghtofLineOne.Equals(LenghtofLineTwo))
+            {
+                Console.WriteLine("First Line and Second Lines are Equal");
+            }
+            else
+            {
+                Console.WriteLine("First Line and Second Lines are not Equal");
+            }
         }
     }
 }
